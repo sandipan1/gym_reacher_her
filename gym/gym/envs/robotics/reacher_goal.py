f861f9c12d40cfcbf01c3500b1fbed5477aa01a5
sandipan <sandipanh05@gmail.com>
2018-04-15 19:02:28 +0530

    physical_reacher

--- gym/gym/envs/robotics/reacher_goal.py
+++ gym/gym/envs/robotics/reacher_goal.py
@@ -12,11 +12,11 @@ class ReacherGoal(robot_env.RobotEnv):
 		self.distance_threshold=distance_threshold
 		self.reward_type=reward_type
 		#self.num_action=2
-		initial_qpos=np.array([0.0,0.0,0.0,0.0])
+		self.initial_qpos=np.array([0.0,0.0,0.0,0.0])
 
 		super(ReacherGoal, self).__init__(
-            model_path=model_path, n_substeps=n_substeps, n_actions=2,
-            initial_qpos=initial_qpos)
+			model_path=model_path, n_substeps=n_substeps, n_actions=2,
+			initial_qpos=self.initial_qpos)
 
 	def compute_reward(self, achieved_goal, goal, info):
 
@@ -34,15 +34,14 @@ class ReacherGoal(robot_env.RobotEnv):
 
 	def _set_action(self, action):
 		assert action.shape == (2,)
-		action = action.copy()  # ensure that we don't change the action outside of this scope
-		pos_ctrl = action[:2]   # robot:body0 and robot:body1 for moving limbs
-
-		pos_ctrl *= 0.05  # limit maximum change in position
-
+		action = np.array(action.copy())  # ensure that we don't change the action outside of this scope
+		action *= 1  
+		action = action.clip(-0.5,0.5)
 		# Apply action to simulation.
 		utils.ctrl_set_action(self.sim, action)
 		#utils.mocap_set_action(self.sim, action)
-
+		for i in range(60):
+			self.sim.step()
 	def _get_obs(self):
 		# positions
 		finger_xpos =self.sim.data.get_body_xpos("robot:fingertip")
@@ -52,9 +51,9 @@ class ReacherGoal(robot_env.RobotEnv):
 		valqpos=np.array(list(map(self.sim.data.get_joint_qpos,a)))
 		valqvel=np.array(list(map(self.sim.data.get_joint_qvel,a)))
 		valqvel=valqvel * dt
-		obj_pos=self.sim.data.get_body_xpos("target")
-		obj_velp = self.sim.data.get_body_xvelp('target') * dt
-		obj_velr = self.sim.data.get_body_xvelr('target') * dt
+		obj_pos=self.sim.data.get_site_xpos("target")
+		obj_velp = self.sim.data.get_site_xvelp('target') * dt
+		obj_velr = self.sim.data.get_site_xvelr('target') * dt
 		obj_rel_pos= obj_pos - finger_xpos
 
 		obs=np.concatenate([finger_xpos,obj_pos,obj_rel_pos,valqpos[:2],finger_xvelp,obj_velp,obj_velr,valqvel[:2]])
@@ -66,28 +65,33 @@ class ReacherGoal(robot_env.RobotEnv):
 			'desired_goal': self.goal.copy(),
 		}
 	def _render_callback(self):
-	    # Visualize target.
-	    #sites_offset = (self.sim.data.site_xpos - self.sim.model.site_pos).copy()
-	    #site_id = self.sim.model.site_name2id('target0')
-	    #self.sim.model.site_pos[site_id] = self.goal - sites_offset[0]
-	    
-	    self.sim.forward()
-
+		# Visualize target.
+		#sites_offset = (self.sim.data.site_xpos - self.sim.model.site_pos).copy()
+		pass
 	def _viewer_setup(self):
 		
 		self.viewer.cam.trackbodyid = 0
 
    
 	def _reset_sim(self):
-		self.sim.set_state(self.initial_state)
+		for i in range(2):
+			self.sim.data.ctrl[i] = self.initial_qpos[i]
+		for i in range(60):
+			self.sim.step()	
 		return True
 # radius =0.2
 	def _sample_goal(self):
 		while (1):
-			qpos = self.initial_state.qpos[-2:] + 200*self.np_random.uniform(low=-.2, high=.2, size=2)
-			if np.linalg.norm(qpos) <= .2:
+			qpos = self.initial_state.qpos[-2:] + .21*np.array([self.np_random.uniform(low=0, high=1),self.np_random.uniform(low=-1, high=1)])
+			if np.linalg.norm(qpos) <= .2 and np.linalg.norm(qpos) >= 0.03:
 				break
+		site_id = self.sim.model.site_name2id('target')
+		self.sim.model.site_pos[site_id][:2] = qpos
+		self.sim.model.site_pos[site_id][-1] = 0.01
 
+		self.sim.forward()
+		for i in range(60):
+			self.sim.step()
 		return qpos
 
 
